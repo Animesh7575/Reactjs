77fe23255c446ee3140d45f15e06d170617fdfbc
Animesh7575 <animeshpandey9999@gmail.com>
2023-08-13 21:44:16 +0530

    Q19

---
+++ Ecommerce website/Q19.js
@@ -0,0 +1,65 @@
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import cartContext from "../store/cart-Context";
+import loginContext from "../store/login-context";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  const loginCtx = useContext(loginContext);
+  const cartCtx = useContext(cartContext);
+
+  const logoutHandler = () => {
+    loginCtx.logout();
+    cartCtx.logoutCartHandler();
+  };
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <nav>
+          <ul>
+            <li>
+              <NavLink activeClassName={classes.active} to="/home">
+                HOME
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName={classes.active} to="/product">
+                PRODUCT
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName={classes.active} to="/about">
+                ABOUT
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName={classes.active} to="/contact">
+                CONTACT
+              </NavLink>
+            </li>
+            {!loginCtx.isloggedIn && (
+              <li className={classes.login}>
+                <NavLink activeClassName={classes.active} to="/login">
+                  LOGIN
+                </NavLink>
+              </li>
+            )}
+            {loginCtx.isloggedIn && (
+              <li className={classes.logout} onClick={logoutHandler}>
+                <NavLink activeClassName={classes.active} to="/login">
+                  LOGOUT
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </nav>
+        <div>
+          <HeaderCartButton />
+        </div>
+      </header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
